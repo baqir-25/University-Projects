1cbaed2a751c5c9ec26f49abdaac9638ad5cc510
baqir-25 <syed.baqir2005@gmail.com>
2026-05-08 13:36:19 +0500

    Delete BookMyCourt/BookMyCourt-backend directory

--- BookMyCourt/BookMyCourt-backend/routes/dealRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const dealController = require("../controllers/dealController");
-
-router.get("/", (req, res) => dealController.listAllActive(req, res));
-
-module.exports = router;
